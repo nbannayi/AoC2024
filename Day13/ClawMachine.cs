50e60dcb0376b56b353e3d526908eeacd05e9673
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2024-12-24 20:24:16 +0000

    Day 13 - an actual ok problem for Fri 13th

---
+++ Day13/ClawMachine.cs
@@ -0,0 +1,48 @@
+﻿namespace Day13
+{
+    public class ClawMachine
+    {
+        public (int, int) ButtonA { get; set; }
+        public (int, int) ButtonB { get; set; }
+        public (long, long) Prize { get; set; }
+
+        /// <summary>
+        /// Create a new claw machine.
+        /// </summary>
+        /// <param name="buttonA">Button A X and Y config.</param>
+        /// <param name="buttonB">Button B X and Y config.</param>
+        /// <param name="prize">Prize X and Y config.</param>        
+        public ClawMachine((int,int) buttonA, (int, int) buttonB, (long, long) prize)
+        {
+            ButtonA = buttonA;
+            ButtonB = buttonB;
+            Prize = prize;         
+        }
+
+        /// <summary>
+        /// Win prize (if possible.)
+        /// </summary>
+        /// <returns>A tuple of No A presses, No B presses and cost. -1's returns if no solution.</returns>
+        public (double, double, double) WinPrize(long offset)
+        {
+            var (a, c) = (ButtonA.Item1, ButtonA.Item2);
+            var (b, d) = (ButtonB.Item1, ButtonB.Item2);
+            var (e, f) = (Prize.Item1 + offset, Prize.Item2 + offset);
+            var det = (double) (a * d - b * c);
+            double an = (e * d - b * f) / (double)det;
+            double bn = (a * f - c * e) / (double)det;
+            // Check all these condituons carefully!
+            var validAnswer = (an - (long)an == 0) && (bn - (long)bn == 0) && an > 0 && bn > 0;
+            return validAnswer ? (an, bn, 3 * an + bn) : (-1, -1, -1);            
+        }
+
+        /// <summary>
+        /// String representation.
+        /// </summary>
+        /// <returns>Claw machine as a string.</returns>
+        public override string ToString()
+        {
+            return $"A: {ButtonA}, B: {ButtonB}, Prize: {Prize}";
+        }
+    }
+}
---
+++ Day13/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+// Day 13: Claw Contraption.
+namespace Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "Day13Input.txt";
+            var clawMachines = GetClawMachines(fileName);
+
+            Console.WriteLine($"Part 1 answer: {GetTotalCost(clawMachines, 0L)}");
+            Console.WriteLine($"Part 2 answer: {GetTotalCost(clawMachines, 10000000000000L)}");
+        }
+
+        // Parse all claw machines into a list.
+        static List<ClawMachine> GetClawMachines(string fileName)
+        {
+            var fileText = File.ReadAllText(fileName);
+            var clawLines = fileText.Split("\n\n");
+            var clawMachines = new List<ClawMachine>();
+            foreach (var clawLine in clawLines)
+            {
+                var clawSettings = clawLine.Split("\n");
+                var buttonATokens = clawSettings[0].Split(" ");
+                var ax = int.Parse(buttonATokens[2].Replace("X+", "").Replace(",", ""));
+                var ay = int.Parse(buttonATokens[3].Replace("Y+", ""));
+                var buttonBTokens = clawSettings[1].Split(" ");
+                var bx = int.Parse(buttonBTokens[2].Replace("X+", "").Replace(",", ""));
+                var by = int.Parse(buttonBTokens[3].Replace("Y+", ""));
+                var prizeTokens = clawSettings[2].Split(" ");
+                var px = long.Parse(prizeTokens[1].Replace("X=", "").Replace(",", ""));
+                var py = long.Parse(prizeTokens[2].Replace("Y=", ""));
+                var clawMachine = new ClawMachine((ax, ay), (bx, by), (px, py));
+                clawMachines.Add(clawMachine);
+            }
+            return clawMachines;
+        }
+
+        static double GetTotalCost(List<ClawMachine> clawMachines, long offset)
+        {
+            ulong totalCost = 0;
+            foreach (var clawMachine in clawMachines)
+            {
+                var (a, b, cost) = clawMachine.WinPrize(offset);
+                if (a >= 0) totalCost += (ulong)cost;
+            }
+            return totalCost;
+        }
+    }
+}
