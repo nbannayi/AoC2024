4bb0b74e628cef34b30690ff80450c8d5a463942
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2025-01-01 20:59:52 +0000

    Day 17 pt 1

---
+++ Day17/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+// Day 17: Chronospatial Computer.
+namespace Day17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up computer and load program.
+            // Parse input data.
+            var inputData = "Day17Input.txt";
+            var (registers, program) = ParseInput(inputData);
+            var (a, b, c) = (registers[0], registers[1], registers[2]);
+
+            // Create and initialise computer.
+            var computer = new ThreeBitComputer(a, b, c);
+            computer.LoadProgram(program);
+
+            // Part 1.            
+            computer.Run(false);
+            Console.WriteLine($"Part 1 answer: {computer.GetOutput()}");            
+
+            // Part 2 - quine.
+            // Target: 2,4,1,1,7,5,4,4,1,4,0,3,5,5,3,0
+            Console.WriteLine($"Part 2 answer: TODO");
+        }
+
+        // Get all details to load the computer.
+        static (int[], string) ParseInput(string filename)
+        {
+            var filelines = File.ReadAllLines(filename);
+            var registers = new int[3];
+            registers[0] = int.Parse(filelines[0].Split(' ')[2]);
+            registers[1] = int.Parse(filelines[1].Split(' ')[2]);
+            registers[2] = int.Parse(filelines[2].Split(' ')[2]);
+            var program = filelines[4].Split(' ')[1];
+            return (registers, program);
+        }
+    }
+}
---
+++ Day17/ThreeBitComputer.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day17
+{
+    public class ThreeBitComputer
+    {     
+        public enum Instruction
+        {
+            ADV,
+            BXL,
+            BST,
+            JNZ,
+            BXC,
+            OUT,
+            BDV,
+            CDV,
+            NUL
+        }
+
+        // Points to next instruction to run.
+        private int _instructionPointer;
+        public int InstructionPointer { get { return _instructionPointer; } }
+
+        // Registers.
+        public long A { get; set; }
+        public long B { get; set; }
+        public long C { get; set; }
+
+        // Program stored on the computer.
+        public List<int> Program { get; set; }
+
+        // Program output.
+        public List<long> Output { get; set; }
+
+        /// <summary>
+        /// Represents a 3 Bit computer (AoC style.)
+        /// </summary>
+        public ThreeBitComputer(long a, long b, long c)
+        {
+            // Empty to begin with.
+            Program = new List<int>();
+            Output = new List<long>();
+
+            _instructionPointer = 0;
+            A = a;
+            B = b;
+            C = c;            
+        }
+
+        /// <summary>
+        /// Load a provided program into the computer.
+        /// </summary>
+        /// <param name="program">Program is supplied as a comma delimited string of instructions e.g. "5,0,5,1,5,4"</param>
+        public void LoadProgram(string program)
+        {
+            Program =
+                program.Split(',').
+                Select(pc => int.Parse(pc)).
+                ToList();
+            Output.Clear();
+        }
+
+        /// <summary>
+        /// Reset the computer but keep the program in memory.
+        /// </summary>
+        public void Reset()
+        {
+            A = 0;
+            B = 0;
+            C = 0;
+            _instructionPointer = 0;
+            Output.Clear();
+        }
+
+        /// <summary>
+        /// Display contents of all registers.
+        /// </summary>
+        public void DisplayRegisters()
+        {
+            Console.WriteLine($"Register A: {A}, ({Convert.ToString(A, 2)})");
+            Console.WriteLine($"Register B: {B}, ({Convert.ToString(B, 2)})");
+            Console.WriteLine($"Register C: {C}, ({Convert.ToString(C, 2)})");
+            Console.WriteLine($"Instruction Pointer: {_instructionPointer}");
+        }
+
+        /// <summary>
+        /// Display program.
+        /// </summary>
+        /// <param name="convertToMnemonics">If true converts program to mnemonics.</param>
+        public void DisplayProgram(bool convertToMnemonics)
+        {
+            var programStringList = new List<string>();
+            for (var i = 0; i <= Program.Count-2; i+=2)
+            {
+                var programOpCode = Program[i];
+                var programOpCodeString = convertToMnemonics ?
+                    ((Instruction)Enum.ToObject(typeof(Instruction), programOpCode)).ToString() :
+                    programOpCode.ToString();                
+                var programOperandString = Program[i + 1].ToString();
+                programStringList.Add(programOpCodeString);
+                programStringList.Add(programOperandString);
+            }
+            var programString = string.Join(",", programStringList);
+            Console.WriteLine($"Program: {programString}");
+        }
+
+        /// <summary>
+        /// Display dump of entire memory.
+        /// </summary>
+        /// <param name="convertToMnemonics">If true converts program to mnemonics.</param>
+        public void MemoryDump(bool convertToMnemonics)
+        {
+            DisplayRegisters();
+            Console.WriteLine();
+            DisplayProgram(convertToMnemonics);
+        }
+
+        /// <summary>
+        /// Return output as a comma delimited string.
+        /// </summary>
+        /// <returns>Output ints separated by commas.</returns>
+        public string GetOutput()
+        {
+            return Output.Count > 0 ?
+                string.Join(",", Output) :
+                "";            
+        }
+
+        /// <summary>
+        /// Run the currently loaded program.
+        /// </summary>
+        /// <param name="debug">Pass debug flag as true to see memory dump.</param>
+        public void Run(bool debug)
+        {
+            if (debug) MemoryDump(false);
+            while (_instructionPointer < Program.Count)
+            {                
+                var instruction = (Instruction)Enum.
+                    ToObject(typeof(Instruction), Program[_instructionPointer]);
+                var operand = Program[_instructionPointer + 1];
+                                
+                switch (instruction)
+                {
+                    case Instruction.ADV:
+                        var comboOperand = GetComboOperand(operand);
+                        A /= (int)Math.Pow(2, comboOperand);
+                        _instructionPointer+=2;
+                        break;
+                    case Instruction.BXL:
+                        B ^= operand;
+                        _instructionPointer+=2;
+                        break;
+                    case Instruction.BST:
+                        comboOperand = GetComboOperand(operand);
+                        B = comboOperand % 8;
+                        _instructionPointer +=2;
+                        break;
+                    case Instruction.JNZ:
+                        if (A != 0)
+                            _instructionPointer = operand;
+                        else
+                            _instructionPointer += 2;
+                        break;
+                    case Instruction.BXC:
+                        B ^= C;
+                        _instructionPointer += 2;
+                        break;
+                    case Instruction.OUT:
+                        comboOperand = GetComboOperand(operand);
+                        var result = comboOperand % 8;
+                        Output.Add(result);
+                        _instructionPointer += 2;
+                        break;
+                    case Instruction.BDV:
+                        comboOperand = GetComboOperand(operand);
+                        B = A / (int)Math.Pow(2, comboOperand);
+                        _instructionPointer += 2;
+                        break;
+                    case Instruction.CDV:
+                        comboOperand = GetComboOperand(operand);
+                        C = A / (int)Math.Pow(2, comboOperand);
+                        _instructionPointer += 2;                        
+                        break;
+                }
+                if (debug)
+                {
+                    //Console.Clear();
+                    Console.WriteLine();
+                    MemoryDump(true);
+                    Console.WriteLine($"Output : {GetOutput()}");
+                }
+            }
+
+            // Get operand for combo instruction mode.
+            long GetComboOperand(long operand)
+            {
+                long comboOperand = operand;
+                switch (operand)
+                {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                        comboOperand = operand;
+                        break;
+                    case 4:
+                        comboOperand = A;
+                        break;
+                    case 5:
+                        comboOperand = B;
+                        break;
+                    case 6:
+                        comboOperand = C;
+                        break;
+                    case 7:
+                        throw new Exception("Combo operand 7 is reserved and should not appear in valid programs.");
+                }
+                return comboOperand;
+            }
+        }
+    }
+}
