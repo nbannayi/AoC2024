7356af1b982e6bd96e86ead7419bafcc7e2f3799
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2025-01-05 12:53:02 +0000

    Day 19 pt 2 - dp

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -21,8 +21,11 @@ namespace Day19
             }
             Console.WriteLine($"Part 1 answer: {noPossibleTowels}");
 
-            // Part 2.            
-            Console.WriteLine($"Part 2 answer: TODO.");            
+            // Part 2.
+            long totalNoTowelCombos = 0;
+            foreach (var towel in towels)
+                totalNoTowelCombos += FindNoTowelCombinations(towel, patterns);
+            Console.WriteLine($"Part 2 answer: {totalNoTowelCombos}");
         }
 
         // Parse linen input.
@@ -35,40 +38,45 @@ namespace Day19
             return (patterns, towels);
         }
 
+        // Get total number of towels that a set of patterns can make.
+        // Need more efficient dynamic programming approach for part 2 vv
+        static long FindNoTowelCombinations(string towel, string[] patterns)
+        {            
+            HashSet<string> patternsSet = new HashSet<string>(patterns);
+            int towelLen = towel.Length;
+            long[] dp = new long[towelLen + 1];
+            dp[0] = 1;
+
+            for (int i = 1; i <= towelLen; i++)
+            {
+                for (int length = 1; length <= i; length++) // Check substrings ending at i
+                {
+                    string substring = towel.Substring(i - length, length);
+                    if (patternsSet.Contains(substring))                    
+                        dp[i] += dp[i - length];                    
+                }
+            }
+
+            return dp[towelLen];
+        }    
+
         // Returns true if given towel is possible, false otherwise.
         static bool IsTowelPossible(string towel, string[] patterns)
         {
-            // Remaining towel, pattern len.
             var stack = new Stack<(string, int)>();
             var visited = new List<(string, int)>();
-
-            // Pare down possible patterns.
-            var tempPatterns = new List<string>();
-            foreach (var pattern in patterns)
-                if (towel.Contains(pattern)) tempPatterns.Add(pattern);
-            patterns = tempPatterns.ToArray();
-
-            // Seed intial attempts.
             var maxPatternLength = patterns.Max(p => p.Length);
-            for (int i = 1; i <= maxPatternLength; i++)
-                stack.Push((towel, i));
-
+            for (int i = 1; i <= maxPatternLength; i++) stack.Push((towel, i));
             while (stack.Count > 0)
             {
                 var (towelRemaining, patternLen) = stack.Pop();                                               
                 var towelSegment = towelRemaining.Substring(0, patternLen);
                 visited.Add((towelRemaining, patternLen));
-
                 if (patterns.Contains(towelSegment))
                 {
                     towelRemaining = towelRemaining.Substring(patternLen);
-
                     // Found a valid combo - so possible in at least 1 way.
-                    if (towelRemaining == "")
-                    {
-                        return true;
-                    }
-
+                    if (towelRemaining == "") return true;
                     // Now try all others.
                     for (int i = 1; i <= maxPatternLength; i++)
                     {
@@ -82,7 +90,6 @@ namespace Day19
                     }
                 }                
             }
-
             // Not possible - exhausted all possibilities.
             return false;
         }
