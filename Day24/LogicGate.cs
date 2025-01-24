bdc937470323537bb4109b1471731e1c8555db48
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2025-01-24 00:16:26 +0000

    Day 24 part 1 - fun problem

---
+++ Day24/LogicGate.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace Day24
+{
+    public enum Operation
+    {
+        AND,
+        OR,
+        XOR
+    }   
+
+    public class LogicGate
+    {
+        public Operation Operation { get; set; }
+        public string InputLabel1 { get; set; }
+        public string InputLabel2 { get; set; }
+        public string OutputLabel { get; set; }
+
+        /// <summary>
+        /// Represents a logic gate.
+        /// </summary>
+        /// <param name="inputLabel1">Input label 1.</param>
+        /// <param name="inputLabel2">Input label 2.</param>
+        /// <param name="operation">Type (AND, OR, XOR.)</param>
+        /// <param name="outputLabel">Input label 2.</param>
+        public LogicGate(string inputLabel1, string inputLabel2, Operation operation, string outputLabel)
+        {
+            InputLabel1 = inputLabel1;
+            InputLabel2 = inputLabel2;
+            Operation = operation;
+            OutputLabel = outputLabel;
+        }
+
+        /// <summary>
+        /// Evaluate the logic gate based on passed dictionary of inputs.
+        /// </summary>
+        /// <param name="inputs">Set of avaulabke inputs.</param>
+        /// <param name="result">Result if it can be evaluated.</param>
+        /// <returns>True if can be evluated based on inputs, false otherwise.</returns>
+        public bool Evaluate(Dictionary<string, bool> inputs, out bool result)
+        {
+            // Exit if can't evaluate yet.
+            if (!inputs.ContainsKey(InputLabel1) || !inputs.ContainsKey(InputLabel2))
+            {
+                result = false;
+                return false;
+            }
+            else
+            {
+                var (input1, input2) = (inputs[InputLabel1], inputs[InputLabel2]);
+                result = Operation switch
+                {
+                    Operation.AND => input1 && input2,
+                    Operation.OR => input1 || input2,
+                    Operation.XOR => input1 ^ input2,
+                    _ => false
+                };
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Represent for keying.
+        /// </summary>
+        /// <returns>Unique strign representation.</returns>
+        public override string ToString()
+        {
+            return $"{InputLabel1} {Operation} {InputLabel2} -> {OutputLabel}";
+        }
+    }
+}
---
+++ Day24/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+// Day 24: Crossed Wires.
+namespace Day24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parse raw inputs.
+            var fileName = "Day24Input.txt";
+            var (inputsDict, gatesDict) = ParseInputs(fileName);
+
+            // Process it all.
+            var processedInputs = ProcessLogicGates(inputsDict, gatesDict);
+
+            // Part 1.
+            Console.WriteLine($"Part 1 answer: {ConvertResultBoolsToInt(processedInputs)}");
+
+            // Part 2.
+            Console.WriteLine($"Part 2 answer: TODO");
+        }
+
+        // Convert a sequence of boolean values representing a binary string to a number.
+        static long ConvertResultBoolsToInt(Dictionary<string, bool> processedInputs)
+        {
+            var kvpZList = processedInputs.
+                Where(kvp => kvp.Key.StartsWith('z')).
+                OrderByDescending(kvp => kvp.Key).                
+                ToList();
+            var resultBinaryString = "";
+            foreach (var kvpZ in kvpZList)            
+                resultBinaryString += kvpZ.Value ? "1" : "0";
+            return Convert.ToInt64(resultBinaryString, 2);
+        }
+
+        // Process all logic gates from inputs and gates dicts.
+        static Dictionary<string, bool> ProcessLogicGates(
+            Dictionary<string, bool> inputsDict,
+            Dictionary<string, LogicGate> gatesDict)
+        {
+            var keysToRemove = new List<string>();            
+            while (gatesDict.Count > 0)
+            {
+                keysToRemove.Clear();
+                foreach (var kvp in gatesDict)
+                {
+                    var gate = kvp.Value;
+                    if (gate.Evaluate(inputsDict, out bool result))
+                    {
+                        keysToRemove.Add(gate.ToString());
+                        inputsDict.Add(gate.OutputLabel, result);
+                    }
+                }                
+                foreach (var key in keysToRemove)
+                    gatesDict.Remove(key);
+            }
+            return inputsDict;
+        }
+            
+        // Parse everything.
+        static (Dictionary<string, bool>, Dictionary<string, LogicGate>) ParseInputs(string inputFile)
+        {
+            var inputText = File.ReadAllText(inputFile);
+            var tokens = inputText.Split("\n\n");
+            inputText = tokens[0];
+            var gatesText = tokens[1];
+            var inputLines = inputText.Split("\n");
+            var gateLines = gatesText.Split("\n");
+
+            // Build input dictionary.
+            var inputsDict = new Dictionary<string, bool>();
+            foreach (var inputLine in inputLines)
+            {
+                tokens = inputLine.Split(": ");
+                inputsDict.Add(tokens[0], tokens[1] == "1");
+            }
+
+            // Build gates dictionary.
+            var gatesDict = new Dictionary<string, LogicGate>();
+            foreach (var gateLine in gateLines)
+            {
+                tokens = gateLine.Split(" -> ");
+                var gateTokens = tokens[0].Split(" ");
+                var inputLabel1 = gateTokens[0];
+                var inputLabel2 = gateTokens[2];
+                var operation = gateTokens[1] switch
+                {
+                    "AND" => Operation.AND,
+                    "OR" => Operation.OR,
+                    "XOR" => Operation.XOR,
+                    _ => throw new Exception("Operation not known.")
+                };
+                var outputLabel = tokens[1];
+                var logicGate = new LogicGate(inputLabel1, inputLabel2, operation, outputLabel);
+                gatesDict.Add(logicGate.ToString(), logicGate);
+            }
+
+            return (inputsDict, gatesDict);
+        }
+    }
+}
